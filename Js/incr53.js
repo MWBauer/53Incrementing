fbc3e39da8a36d226498e590560e5770be45ca58
Marc <marc.jahservant@gmail.com>
2018-07-18 00:07:52 -0400

    Initial Commit

---
+++ Js/incr53.js
@@ -0,0 +1,12 @@
+var c;
+c = 5;
+document.writeln("<h3>Postincrementing</h3>");
+document.writeln(c);
+document.writeln("<br/>" + c++);
+document.writeln("<br/>" + c);
+
+c = 5;
+document.writeln("<h4>Preincrementing</h4>");
+document.writeln(c);
+document.writeln("<br/>" + ++c);
+document.writeln("<br/>" + c);
